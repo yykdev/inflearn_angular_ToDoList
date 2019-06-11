945482ec7fe460420fee2d7741ede51f998c4528
yykdev <assavictory@gmail.com>
2019-06-11 19:02:43 +0900

    모듈 추가 실습

---
+++ todo-app/src/app/greeting.ts
@@ -0,0 +1,5 @@
+export class Greeting {
+    sayHello ( name: string ) {
+        console.log('hello ' + name);
+    }
+}
--- todo-app/src/main.ts
+++ todo-app/src/main.ts
@@ -4,9 +4,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import { Greeting } from './app/greeting';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+let g = new Greeting();
+g.sayHello('nouveau');
