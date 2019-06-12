c0dca1fc21475ae33525869aae6c94de58972dcf
yykdev <assavictory@gmail.com>
2019-06-12 17:43:28 +0900

    컴포넌트 단위 css : add-todo/add-todo.component.ts

--- todo-app/src/app/todo/todos/add-todo/add-todo.component.ts
+++ todo-app/src/app/todo/todos/add-todo/add-todo.component.ts
@@ -3,10 +3,37 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-add-todo',
   template: `
+    <button (click)="addTodo(newText)">+</button>
     <input type="text" name="" id="" placeholder="할 일 추가" [(ngModel)]="newText">
-    <button (click)="addTodo(newText)">ADD</button>
   `,
-  styles: []
+  styles: [`
+    :host {
+      display:block;
+      padding: 16px 16px 16px 10px;
+      background-color: white;
+    }
+
+    input {
+      display: inline-block;
+      font-size: 18px;
+      border: none;
+    }
+
+    input:focus, button:focus {
+      outline: none;
+    }
+
+    button {
+      width: 24px;
+      height: 24px;
+      border-radius: 24px;
+      color: white;
+      font-size: 16px;
+      line-height: 17px;
+      border: 1px solid dimgray;
+      background-color: darkblue;
+    }
+  `]
 })
 export class AddTodoComponent implements OnInit {
 
