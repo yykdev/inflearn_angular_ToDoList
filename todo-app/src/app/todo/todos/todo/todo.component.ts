7f8eaa8f5ab32ac616c074eb9605687b25cbe773
yykdev <assavictory@gmail.com>
2019-06-12 17:43:47 +0900

    컴포넌트 단위 css : todo/todo.component.ts

--- todo-app/src/app/todo/todos/todo/todo.component.ts
+++ todo-app/src/app/todo/todos/todo/todo.component.ts
@@ -4,9 +4,53 @@ import { Todo } from '../../share/todo.model';
 @Component({
   selector: 'app-todo',
   template: `
-    <input type="checkbox" [checked]="todo.done"> {{ todo.text }}
+    <input type="checkbox" [checked]="todo.done"><label> {{ todo.text }}</label>
   `,
-  styles: []
+  styles: [`
+    :host {
+      display: block;
+      padding : 16px;
+      color: darkgray;
+      background-color: white;
+    }
+
+    input {
+      position: relative;
+    }
+
+    input:before {
+      content: "";
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background-color: white;
+      border-radius: 20px;
+      position: absolute;
+      top: -6px;
+      left: -8px;
+      border: 1px solid dimgray;
+    }
+
+    input:checked:after {
+      content: '\\2713';
+      display: inline-block;
+      font-size: 18px;
+      width: 20px;
+      height: 20px;
+      border-radius: 20px;
+      position: absolute;
+      top: -6px;
+      left: -8px;
+      border: 1px solid dimgray;
+      background-color: dimgray;
+      text-align: center;
+      color: white;
+    }
+
+    input:checked + label {
+      text-decoration: line-through;
+    }
+  `]
 })
 export class TodoComponent implements OnInit {
 
